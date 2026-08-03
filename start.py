2ea50ce47db7b13b37c75997572974a70df44b6f
Sanatan Bramhane <bsanatan51@gmail.com>
2026-08-03 15:32:36 +0530

    added escape sequence examples

--- start.py
+++ start.py
@@ -1,5 +1,29 @@
+#variables
+print("Example for Variables:")
 name = "Sanatan Bramhane"
 print("Hello ", name)
 age = 24
 
-print(name + " age is ", age)
+print("----------------------------------------")
+
+#print statment
+print("Example for print function:")
+print(name + " age is", age, "years old")
+
+#escape sequences
+print("Example for Escape Sqeuences:")
+print(r'1. \n', "this is used to print a new line also known as new line character")
+print(r'2. \t', "this is used to give a extended space in a sentence as a tab")
+print(r'3. \\', "this is used to print a single character")
+print(r'4. \'', "this is used to print a single quote in the sentence")
+print(r'5. \"', "this is used to print a double quote in the sentence")
+print("Hi. \nHow are you!")
+print("Hi. \tHow are you!")
+print("Hi. \\How are you!")
+print("Hi. This is sanatan\'s house.")
+print("Hi. This is you are calling at Bramhane \"nHow are you!")
+
+
+
+
+
