6a5b05352cfb8a73e16382dd6a2fe685fa782938
Sanatan Bramhane <bsanatan51@gmail.com>
2026-08-06 15:04:12 +0530

    numbers

---
+++ numbers.py
@@ -0,0 +1,46 @@
+print("-------------------------------------------")
+#we can directly print numbers using print
+print("Print Numbers using Print function")
+print(78)
+
+print("-------------------------------------------")
+
+#assign a number to a variable and print
+print("Assigning a number to a variable")
+number = 78
+print("the number is ", number) #in this case we cannot use string concatination to concatinate the string and the integer
+
+print("-------------------------------------------")
+
+#using absolute function
+#this function converts a negative value to positive and keeps positive value as it is
+print("Using absolute function")
+num1 = -5
+print(abs(num1))
+
+print("-------------------------------------------")
+
+#using max function
+#this function returns the maximum value from all the values passed inside the function
+print("using max function")
+print(max(1,3,5,6,77,8))
+
+
+
+print("-------------------------------------------")
+
+#using min function
+#this function returns the minimum value from all the values passed inside the function
+print("using min function")
+print(min(1,3,5,6,77,8))
+
+print("-------------------------------------------")
+
+#using round function
+#this function rounds the floating value to the closest of the integer
+print("using round function")
+print("the rounded integer is ", round(4.7), "this rounds to the next integer that is 5")
+print("the rounded integer is ", round(4.4), "this rounds to the previous integer that is 4")
+
+print("-------------------------------------------")
+
